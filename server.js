402ca788ea7df5f1a8e9de90a08b064f6d9151e7
iwen-sue <grace2268@hotmail.com>
2024-09-27 02:15:52 -0700

    root server

---
+++ server.js
@@ -0,0 +1,11 @@
+let message = require('./lab3/lang/en/en');
+let http = require('http');
+let url = require('url');
+
+http.createServer((req, res) => {
+    console.log('Incoming request:', req.url);
+    // if (req.url === '/COMP4537/test') {
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     return res.end('test');
+    // }
+}).listen(process.env.PORT || 8080);
