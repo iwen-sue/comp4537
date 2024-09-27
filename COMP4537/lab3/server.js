18a59e43ece8791342bc7f5591ee057f26fe92fc
iwen-sue <grace2268@hotmail.com>
2024-09-27 02:10:07 -0700

    test url

--- COMP4537/lab3/server.js
+++ COMP4537/lab3/server.js
@@ -3,12 +3,8 @@ let http = require('http');
 let url = require('url');
 
 http.createServer((req, res) => {
-    if (req.url === '/COMP4537/lab3/getDate') {
+    if (req.url === '/COMP4537/test') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        return res.end('getDate');
-    } if (req.url === '/COMP4537/lab3/writeFile') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        return res.end('writeFile');
+        return res.end('test');
     }
-
 }).listen(process.env.PORT || 8080);
